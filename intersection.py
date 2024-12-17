d4f943be86fa114319bf0c828377d21e170951c8
swimdoraemon <indoaradhya@gmail.com>
2024-12-17 19:26:29 +0900

    lalala

---
+++ intersection.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+setx = {"purple", "blue"}
+print("Oringinal set elements:")
+print(setx)
+print(setx)
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setx)
